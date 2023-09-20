789bb26d429fbe6d6734f3b495741f085788baab
Vinayak MK <48713290+VinayakMK@users.noreply.github.com>
2023-09-21 00:08:22 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+//document.getElementById("firstTitle").innerHTML = "Title from the Script"
+
+//let res = document.getElementsByClassName("text");
+
+//console.log(res[0].innerHTML = "Updated Link");
+
+//let res = document.getElementsByTagName("h2");
+
+//let res = document.querySelector("a").innerHTML = "Updated content";
+
+//let res = document.querySelectorAll("a");
+
+//let res = document.forms["form1"];
+
+//let res = document.getElementById("firstTitle").innerHTML = "Modified Title";
+
+let res = document.getElementById("dateContainer").innerHTML = `Current Date and Time : ${Date()}`;
+
+console.log(res);
